59f3fea370ed55b8ab97ae712b67961f0930f8ce
SarahHerberichs <sarah.herberichs@neuf.fr>
2025-09-09 16:50:14 +0200

    railway73

--- woofwalks_back/docker/nginx/entrypoint.sh
+++ woofwalks_back/docker/nginx/entrypoint.sh
@@ -10,14 +10,10 @@ cd /var/www/html
 envsubst '$PORT' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf.temp
 mv /etc/nginx/conf.d/default.conf.temp /etc/nginx/conf.d/default.conf
 
-# 0️⃣ Vider le cache pour éviter les problèmes de configuration ou de routes
 php bin/console cache:clear --no-warmup
-
-# 1️⃣ Créer la base si elle n'existe pas
 php bin/console doctrine:database:create --if-not-exists --no-interaction
-
-# 2️⃣ Appliquer les migrations pour créer les tables
 php bin/console doctrine:migrations:migrate --no-interaction
+
 
 
 # 3️⃣ Lancer PHP-FPM et Nginx
