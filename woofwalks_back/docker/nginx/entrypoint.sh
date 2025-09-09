26ce01e34a3e4e78b1f99d9371170eb2fc0d9eca
SarahHerberichs <sarah.herberichs@neuf.fr>
2025-09-09 12:47:15 +0200

    railway31

--- woofwalks_back/docker/nginx/entrypoint.sh
+++ woofwalks_back/docker/nginx/entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 set -e
-php-fpm8.2 -F &   # -F = foreground, & pour laisser le script continuer
+
+echo "=== Contenu de /usr/share/nginx/html au démarrage ==="
+ls -l /usr/share/nginx/html
+echo "=== Début index.html ==="
+head -n 10 /usr/share/nginx/html/index.html
+
+php-fpm8.2 -F &   # PHP-FPM en foreground
 nginx -g 'daemon off;'
