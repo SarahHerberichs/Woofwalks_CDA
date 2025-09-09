70a9bab8fc8633d190b2b16cf04680fc00035a10
SarahHerberichs <sarah.herberichs@neuf.fr>
2025-09-09 13:19:09 +0200

    railway31

--- woofwalks_back/docker/nginx/entrypoint.sh
+++ woofwalks_back/docker/nginx/entrypoint.sh
@@ -4,12 +4,15 @@ set -e
 echo "=== Contenu de /usr/share/nginx/html ==="
 ls -l /usr/share/nginx/html
 
-echo "=== Contenu de /etc/nginx/conf.d/default.conf ==="
+echo "=== Contenu de /etc/nginx/conf.d/default.conf avant modification ==="
 cat /etc/nginx/conf.d/default.conf
 
-# Remplacer le port Nginx par celui de Railway
-sed -i "s/listen 80;/listen ${PORT};/" /etc/nginx/conf.d/default.conf
+# Remplacer le port par la variable Railway
+sed -i "s/listen .*/listen ${PORT};/" /etc/nginx/conf.d/default.conf
+
+echo "=== Contenu de /etc/nginx/conf.d/default.conf après modification ==="
+cat /etc/nginx/conf.d/default.conf
 
 # Lancer PHP-FPM et Nginx
-php-fpm -F &       # PHP-FPM en arrière-plan
-nginx -g 'daemon off;'  # Nginx au premier plan
+php-fpm -F &          # PHP-FPM en arrière-plan
+nginx -g 'daemon off;' # Nginx en avant-plan
