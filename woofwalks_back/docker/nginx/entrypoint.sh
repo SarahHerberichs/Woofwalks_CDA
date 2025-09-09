432006683186847781c31e50b78b7b013991f175
SarahHerberichs <sarah.herberichs@neuf.fr>
2025-09-09 14:09:07 +0200

    railway57

--- woofwalks_back/docker/nginx/entrypoint.sh
+++ woofwalks_back/docker/nginx/entrypoint.sh
@@ -3,11 +3,10 @@ set -e
 
 # Remplacer ${PORT} par la vraie variable d'environnement
 PORT=${PORT:-8080}
-# Utiliser envsubst pour substituer les variables d'environnement dans le fichier de configuration de Nginx
-# Cette commande est plus robuste que sed pour ce genre de tâche
+# On utilise la substitution de variable pour que la configuration Nginx prenne en compte le port
 envsubst '$PORT' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf.temp && \
 mv /etc/nginx/conf.d/default.conf.temp /etc/nginx/conf.d/default.conf
 
-# Lancer PHP-FPM et Nginx
+# Lancer PHP-FPM et Nginx en spécifiant le bon fichier de configuration
 php-fpm -F &          # PHP-FPM en arrière-plan
-nginx -g 'daemon off;' # Nginx en avant-plan
+nginx -g 'daemon off;' -c /etc/nginx/nginx.conf # Nginx en avant-plan
